b4c6d00da70483c5b57f89b07e06898807869a49
saadmnhm <saadmnhm@gmail.com>
2026-02-28 19:49:30 +0000

    dev

--- resources/views/livewire/admin/aside.blade.php
+++ resources/views/livewire/admin/aside.blade.php
@@ -107,21 +107,10 @@
                         </a>
                         @endcanmodule
 
-                        @canmodule('rh')
-                        <a href="{{ route('admin.rh.index') }}" class="block px-4 py-2 rounded hover:bg-gray-100 {{ request()->routeIs('admin.rh*') ? 'bg-gray-100 font-medium' : '' }}">
-                            <i class="ri-team-line mr-1"></i> Gestion RH
-                        </a>
-                        @endcanmodule
 
                         @canmodule('gestion_roles')
                         <a href="{{ route('admin.roles.index') }}" class="block px-4 py-2 rounded hover:bg-gray-100 {{ request()->routeIs('admin.roles*') ? 'bg-gray-100 font-medium' : '' }}">
                             <i class="ri-shield-user-line mr-1"></i> Gestion des Rôles
-                        </a>
-                        @endcanmodule
-                        
-                        @canmodule('association_parameters')
-                        <a href="{{ route('admin.association.parameters') }}" class="block px-4 py-2 rounded hover:bg-gray-100 {{ request()->routeIs('admin.association*') ? 'bg-gray-100 font-medium' : '' }}">
-                            <i class="ri-settings-3-line mr-1"></i> Paramètres Association
                         </a>
                         @endcanmodule
 
